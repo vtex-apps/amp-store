06737790a68f12cb4edab679ba276dc9e3a70caf
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-08-14 13:57:52 -0300

    add sample component

---
+++ react/BaseComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import * as Amp from 'react-amphtml'
+
+const BaseComponent = () => {
+  return (
+    <React.Fragment>
+      <h1>hello from amp page!</h1>
+      <Amp.AmpImg
+        specName="default"
+        width="400"
+        height="205"
+        src="http://brand.vtex.com/static/media/logo.2f3fc60b.svg"
+      />
+    </React.Fragment>
+  )
+}
+
+export default BaseComponent
